28bd69c4d70d797ad46dfa5de2d34c8ae9db5197
Octavian Badea <octavio.badea@gmail.com>
2023-10-03 13:28:58 +0200

    Añadida barra top con nombre y logo. Añadida animación de giro al logo central con el hover. Cambio general del aspecto. Botón colocado a la derecha con una animación de pulso.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ const option = document.getElementById("desplegable_frutas");
 option.addEventListener("change", function () {
   const eleccion = option.value;
   console.log(eleccion);
+  return eleccion;
 });
